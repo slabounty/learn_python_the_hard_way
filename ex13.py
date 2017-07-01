ab5916d26bad0765eae629ece60dd9357ded5b5e
Scott LaBounty <slabounty@gmail.com>
2017-07-01 16:12:34 -0700

    Exercise 13

---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script, first, second, third = argv
+
+additional = raw_input("How about an additional value?: ")
+
+print "The script is called: ", script
+print "Your first variable is: ", first
+print "Your second variable is: ", second
+print "Your third variable is: ", third
+print "Your additional variable is: ", additional
